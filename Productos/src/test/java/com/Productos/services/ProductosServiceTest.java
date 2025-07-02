0a3fe703fec8acf6a1854a963de022fe568387ba
Dante Schacht <dantesch10@gmail.com>
2025-07-01 20:43:50 -0400

    Se agrego la prueba con junit al Service

---
+++ Productos/src/test/java/com/Productos/services/ProductosServiceTest.java
@@ -0,0 +1,30 @@
+package com.Productos.services;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductosServiceTest {
+    @Test
+    void testActualizar() {
+
+    }
+
+    @Test
+    void testCrear() {
+
+    }
+
+    @Test
+    void testEliminar() {
+
+    }
+
+    @Test
+    void testListar() {
+
+    }
+
+    @Test
+    void testObtenerPorId() {
+
+    }
+}
